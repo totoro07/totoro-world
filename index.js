8e407d6edf916a4dc4edb73f30424a8c3f612805
totoro <lovesua2007@gmail.com>
2023-07-13 20:14:27 +0900

    totoro is love

---
+++ index.js
@@ -0,0 +1,6 @@
+setInterval(()=>{
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+    a.src='https://play-lh.googleusercontent.com/proxy/bqpbXejtqoLRKtu2PBwZIRU-0LYQkDCxZ1PSzoXheXrfksxMoTXeFJACt9PnQqn30MCbylEzOZM76lejnEGglYjlxl7gVKmMzRPtk2ZejNcB_iNkw-Mf6gaymWf6kqScqfjwvdui3MuYqEqL2Y0VNhe9kQ_D8sfDmN3oig=s3840-w3840-h2160'
+})
+}, 500)
